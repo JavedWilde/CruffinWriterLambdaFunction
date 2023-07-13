f8c1ade8ec130963c2dbd3d1c7becce21772568d
Javed <javedwilde@gmail.com>
2023-07-14 03:22:32 +0530

    push

--- Demo.py
+++ Demo.py
@@ -5,6 +5,7 @@ fontFile = f'Fonts/SVGFONT ({0}).svg' #change numbers for different fonts, 0 - 1
 gcode = Helpers.GetGcode(text = 'Happy Birthday Dude dabs', fontFile = fontFile, xOffset = 0, yOffset = 0, max_bed_x = 60, max_bed_y = 20, move_speed = 300, cut_speed = 300, letterLimit = 25, arThres = 8)
 
 # the generate svg file is upside down, i kept it that way since it was easier to just flip and get true 0 starting point on x and y, no random offsets, for viewing on web, just scale by -1 on the y
+# svg file is saved as svg.svg
 
 # font files are files converted from ttf to svg glyphs using an online tool, if we wanna add a font, just need to drag drop and click a button
 # xOffset, yOffset is the final offset applied to the print, we will calculate this based on the size and shape of the chocolate plate
