230a2f2cd9853e16d79ef2efbc89fd0e41e71093
Javed <javedwilde@gmail.com>
2023-07-14 03:04:13 +0530

    push

--- Helpers.py
+++ Helpers.py
@@ -201,7 +201,6 @@ def GetMultiLine(text, fontDict):
 
     return paths
 
-
 def GetGcode(text = 'Enjoy', fontFile = f'Fonts/SVGFONT ({0}).svg', xOffset = 0, yOffset = 0, max_bed_x = 60, max_bed_y = 20, move_speed = 300, cut_speed = 300, letterLimit = 25, arThres = 8):
     
     fontDict = GetGlyphDictionary(fontFile)
